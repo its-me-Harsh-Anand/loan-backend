aa30250a23547df705e704fe1b49b3a71876a83a
its-me-Harsh-Anand <harsha.ug20.ece@nitp.ac.in>
2022-05-23 16:21:50 +0530

    add index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const app = require('./app');
 const PORT = process.env.PORT || 8000
 
+app.get('/', (req, res)=>{
+    res.send({name : "Harsh"})
+})
+
 app.listen(PORT, () => {
     console.log('Listening on port 8000');
 })
