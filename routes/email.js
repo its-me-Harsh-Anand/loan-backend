a65aafd721a50e0bf22a8ae23e43fad56aa2c478
its-me-Harsh-Anand <harsha.ug20.ece@nitp.ac.in>
2022-05-27 22:09:38 +0530

    mailing and messaging recreated

--- routes/email.js
+++ routes/email.js
@@ -2,46 +2,29 @@ var nodemailer = require('nodemailer');
 const router = require("express").Router()
 const axios = require('axios')
 
-router.route('/sendemail/:id').post((req, res)=>{
-    const id = req.params.id
-    req.header("Access-Control-Allow-Origin", "true")
-    axios.get(`${process.env.BACKEND_URL}/user/${id}`)
-    .then(res => res.data)
-    .then(data => {
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-              user: `${process.env.EMAIL}`,
-              pass: `${process.env.PASSWORD}`
-            }
-          });
-          
-          var mailOptions = {
-            from: `${process.env.EMAIL}`,
-            to: `${data.user_email}`,
-            subject: 'LoanTark Registration Complete',
-            text: 
-            `Dear ${data.user_fname},
-            Your application for Business Loan and loan amount ${data.loan_amount} is under process by LoanTark.Your Interest Rate 7.5 % per anumm and EMI ${data.loan_emi} for tenure ${data.loan_tenure} years. Submit all Below Mentioned KYC document earlier
-            1.Aadhaar  Card
-            2.Pan Card
-            3.Self Photograph
-            4.Bank Statement
-            5.Two References
-            For More Information
-            Visit http://loantark.org
-            Or WhatsApp Us at +19403705935`
-          };
-          
-          transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
-          
-    }).catch(err => res.json(err))
+router.route('/sendemail').post((req, res)=>{
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: `${process.env.EMAIL}`,
+      pass: `${process.env.PASSWORD}`
+    }
+  });
+  
+  var mailOptions = {
+    from: `${process.env.EMAIL}`,
+    to: req.body.to,
+    subject: 'LoanTark Registration Complete',
+    text: req.body.text
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 
 })
 
--- routes/message.js
+++ routes/message.js
@@ -13,7 +13,7 @@ router.route("/sendmessage").post((req, res) => {
     .create({
       body: req.body.body,
       from: "+19403705935",
-      to: req.body.to //put here your twillio verified number with country code, for ex:(+918234567890), in order to get message
+      to: `+91${req.body.to}`
     })
     .then(() => {
       res.send(JSON.stringify({ success: true }));
@@ -25,32 +25,20 @@ router.route("/sendmessage").post((req, res) => {
   
 });
 
-router.route("/sendwhatsapp/:id").post((req, res) => {
-  const id = req.params.id;
-  req.header("Access-Control-Allow-Origin", "true");
-  res.header("Content-Type", "application/json");
-  axios
-    .get(`${process.env.BACKEND_URL}/user/${id}`)
-    .then((res) => res.data)
-    .then((data) => {
-      client.messages
-        .create({
-          body: `Dear ${data.user_fname},
-            Your application for Business Loan and loan amount ${data.loan_amount} is under process by LoanTark.Your Interest Rate 7.5 % per anumm and EMI ${data.loan_emi} for tenure ${data.loan_tenure} years. Submit all Below Mentioned KYC document earlier
-            1.Aadhaar  Card
-            2.Pan Card
-            3.Self Photograph
-            4.Bank Statement
-            5.Two References
-            For More Information
-            Visit http://loantark.org
-            Or WhatsApp Us at +19403705935`,
-          from: "whatsapp:+19403705935",
-          to: `whatsapp:+91${data.user_mobile}`, //put here your twillio verified number with country code, for ex:(+918234567890), in order to get message
-        })
-        .then((message) => console.log(message))
-        .catch((err) => console.error(err));
+router.route("/sendwhatsapp").post((req, res) => {
+  res.header('Content-Type', 'application/json')
+    client.messages
+    .create({
+      body: req.body.body,
+      from: "whatsapp:+19403705935",
+      to: `whatsapp:+91${req.body.to}`
     })
-    .catch((err) => res.json(err));
+    .then(() => {
+      res.send(JSON.stringify({ success: true }));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+    });
 });
 module.exports = router;
